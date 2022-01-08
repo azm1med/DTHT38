8685fb460c6cfb80086c5af68fbfa036d182ac38
azm1med <batuongweb@gmail.com>
2022-01-08 12:14:17 +0700

    slider anh Pagination in details-page--update

--- assets/js/details.js
+++ assets/js/details.js
@@ -14,3 +14,55 @@ function details__desc() {
     }
 }
 details__desc();
+
+
+var slideIndex = 1;
+    relics__show(slideIndex);
+
+function relics__plus(n) {
+    relics__show(slideIndex += n);
+}
+
+function relics__current(n) {
+    relics__show(slideIndex = n);
+    // window.setInterval(function() {
+    //     relics__show(slideIndex = n + 1);
+    // }, 5000); 
+}
+
+function relics__show(n) {
+    var i;
+    var slides = document.getElementsByClassName("details__relics-lib-view-img");
+    var dots = document.getElementsByClassName("details__relics-lib-item");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+var items = $(".details__relics-media .details__relics-media-item");
+var numItems = items.length;
+var perPage = 5;
+
+items.slice(perPage).hide();
+
+$('#relics__control').pagination({
+    items: numItems,
+    itemsOnPage: perPage,
+    prevText: "<i class='fal fa-chevron-left'></i>",
+    nextText: "<i class='fal fa-chevron-right'></i>",
+    onPageClick: function (pageNumber) {
+        var showFrom = perPage * (pageNumber - 1);
+        var showTo = showFrom + perPage;
+        items.hide().slice(showFrom, showTo).show();
+        $('#relics__today').html(pageNumber);
+    }
+});
+$('#relics__page').html( $('#relics__control').pagination('getPagesCount'));
+$('#relics__today').html( $('#relics__control').pagination('getCurrentPage'));
